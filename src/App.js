d172f080889d19b4763cdadfcf106046bbbd44bf
Marvin <marvinkamwenjih@gmail.com>
2023-01-02 17:53:13 +0300

    starting on expenses

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Expenses from './components/Expenses';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <Expenses />
     </div>
   );
 }
